58698b84e7b1a705794ff2b3e66cac6e476da38c
daidenghui <3276336032@qq.com>
2021-07-22 10:03:48 +0800

    两数之和.cpp 添加描述

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -3,13 +3,23 @@
  *
  * [1] 两数之和
  */
+#include <iostream>
 #include <map>
 #include <vector>
-#include <iostream>
 using std::cout;
 using std::endl;
 using std::map;
 using std::vector;
+
+/*
+ * Solution description:
+ * 使用辅助空间map将原数组内容作为关键字key，下标作为值value
+ * 首先将第一个数组第一个元素放入map -- 匹配需要两个以元素，所以第一个直接加入map
+ * 进入循环，i从1开始。计算数target与组元素的差值difference_value，并作为关键字在map中查找
+ * 如果未查找到将将原数组内容作为关键字key，下标作为值value传入map
+ * 如果查找到返回差值difference_value对应map的值与i的值
+ * 循环完成没有退出，证明未查找到满足条件的点，返回空
+ */
 
 // @lc code=start
 class Solution
