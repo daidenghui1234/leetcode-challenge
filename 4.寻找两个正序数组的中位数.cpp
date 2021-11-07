60a222afdcb50030915204d5e9c38e4d5e82ee3d
‘daidenghui’ <daidenghuiwst@163.com>
2021-11-07 19:55:46 +0800

    4 寻找中位数

--- 4.寻找两个正序数组的中位数.cpp
+++ 4.寻找两个正序数组的中位数.cpp
@@ -16,19 +16,18 @@ class Solution
 public:
     double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
     {
-        vector<int> sum = nums1;
-        double ret;
-        for (int i = 0; i < nums2.size(); i++)
-        {
-            sum.push_back(nums2[i]);
+        float result;
+        for(int i=0;i<nums2.size();i++){
+            nums1.push_back(nums2[i]);
         }
-        sort(sum.begin(), sum.end());
-        int endlen = sum.size();
-        if(endlen%2==0){
-            ret = (sum[endlen/2]+sum[endlen/2+1])/2.0;
+        sort(nums1.begin(),nums1.end());
+        int m=nums1.size();
+        if(m%2){
+            result=nums1[m/2];
+        }else{
+            result=(float(nums1[m/2-1])+float(nums1[m/2]))/2;
         }
-        ret = sum[endlen/2];
-        return ret;
+    return result;
     }
 };
 // @lc code=end
