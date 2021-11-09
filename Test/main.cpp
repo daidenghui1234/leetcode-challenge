fcc4b73e2cab1a4b9b1b1ac5e6ba88f60f65cb2c
Dai Denghui <daidenghuiwst@163.com>
2021-11-09 13:46:55 +0800

    查找

--- Test/main.cpp
+++ Test/main.cpp
@@ -3,35 +3,8 @@
 #include <string>
 using namespace std;
 
-string replaceSpace(string s)
-{
-    size_t found = s.find(" ");
-    while (s.find(" ") != string::npos)
-    {
-        s.replace(found, 1, "%20");
-        found = s.find(" ");
-    }
-    return s;
-}
-string reverseLeftWords(string s, int n)
-{
-    char tempc;
-    int len = s.length()-1;
-    for(int i =0; i<n; i++){
-        tempc = s[0];
-        for(int j = 0; j<len; j++){
-            s[j] = s[j+1];
-        }
-        s[len] = tempc;
-    }
-    
-    return s;
-}
+#include <TEST.H>
 
-void test01()
-{
-    
-}
 int main(int argc, char const *argv[])
 {
     reverseLeftWords("123456", 2);
